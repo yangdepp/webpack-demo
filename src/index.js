617b52afc3beefe2589339bf5da47d9b9493b4a3
yangdepu <yangdepp@163.com>
2019-09-25 18:20:18 +0800

    字体图标

--- src/index.js
+++ src/index.js
@@ -1,12 +1,3 @@
-import testImg from './test.jpg';
-import style from './index.scss';
-import createTest from './createTest';
-
-createTest();
-
-const img = new Image();
-img.src = testImg;
-img.classList.add(style['img-test']);
-
 const root = document.getElementById('root');
-root.append(img);
+
+root.innerHTML = ('<div class="test">abc</div>')
